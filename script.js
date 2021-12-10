24e86ad9e79a7d45908364e7b8b61440fc2f3a81
imo-jpg <imo.todd@gmail.com>
2021-12-09 20:02:23 -0500

    cleans up code

--- script.js
+++ script.js
@@ -17,12 +17,14 @@ const tooltip = d3.select("body")
             .append("div")
             .attr("class", "tooltip");
 
+//formats the dates in the CSV for easy reading
 let dateFormat = d3.timeParse("%x");
-let justYear = d3.timeFormat("%y");
+let justYear = d3.timeFormat("%Y");
 
 //reads in my dataset
 d3.csv("berlin_wall_deaths.csv").then(function(dataset) {
 
+    //adjusts the values in the dataset so they're more usable
     dataset.forEach(function(d) {
         d.Age = parseFloat(d.Age);
         d.year = d.dod;
@@ -49,7 +51,6 @@ d3.csv("berlin_wall_deaths.csv").then(function(dataset) {
                 }
             }
        };
-
    
     let allAges = ageNest.concat(newAges);
     allAges.sort((a,b) => d3.ascending(parseFloat(a.key), parseFloat(b.key)));
@@ -63,9 +64,7 @@ d3.csv("berlin_wall_deaths.csv").then(function(dataset) {
         .append("div")
         .attr("class", "ageGroup");
     
-
-    //sets up the nested ages for building the histogram
-
+    //sets up the nested years for building the histogram
     let yearNest = d3
         .nest()
         .key(d => d.year)
@@ -98,7 +97,7 @@ d3.csv("berlin_wall_deaths.csv").then(function(dataset) {
     let colors = ["#34A8F9", "#978802", "#EA8F00", "#C1738D", "#288CD2", "#B7A40A", "#C07924", "#DD8FA9", "#2D6FA3", "#615718", "#9B5E00", "#7F4B5B", "#2D5A83"];
     let causes = ["Shot", "Drowned", "Train", "Suicide", "Fall", "Injuries", "Shot-Accident", "Suffocation", "Accident", "Balloon crash", "Bludgeoned", "Pneumonia", "Unknown"]
     
-    // footer.html(`<p>${names}<p>`);
+    //adds the list of names to the edges of the page (this shouldn't really be called "footer", sorry)
     for (let i = 0; i < dataset.length; i++) {
         footer.append("p").html(dataset[i].Names)
         .style("color", "#333333")
@@ -112,8 +111,9 @@ d3.csv("berlin_wall_deaths.csv").then(function(dataset) {
         });
     }
 
-    window.onscroll = function() {changeColor()};
 
+    //sets up an event listener and a function so the names change color when you scroll past a certain point
+    window.onscroll = function() {changeColor()};
 
     function changeColor() {
       if (document.body.scrollTop > 2840 || document.documentElement.scrollTop > 2840) {
@@ -176,31 +176,31 @@ d3.csv("berlin_wall_deaths.csv").then(function(dataset) {
         .on("mouseleave", tooltipLeave)
         .on("touchstart", tooltipEnter)
         .on("touchmove", tooltipMove)
-        .on("touchend", tooltipLeave)
-        ;
+        .on("touchend", tooltipLeave);
         
-        //this loops adds the rest of the charts (filtered by cause of death)
-        for (let i = 0; i < 13; i++) {
-            chart[i].selectAll(".person")
-            .data(dataset.filter(function(d) { return d.Cause == causes[i] }))
-            .enter()
-            .append("div")
-            .attr("class", "person")
-            .style("background-color", colors[i])
-            .on("mouseenter", tooltipEnter)
-            .on("mousemove", tooltipMove)
-            .on("mouseleave", tooltipLeave)
-            .on("touchstart", tooltipEnter)
-            .on("touchmove", tooltipMove)
-            .on("touchend", tooltipLeave)
-            ;
-        };
+    //this loops adds the rest of the charts (filtered by cause of death)
+    for (let i = 0; i < 13; i++) {
+        chart[i].selectAll(".person")
+        .data(dataset.filter(function(d) { return d.Cause == causes[i] }))
+        .enter()
+        .append("div")
+        .attr("class", "person")
+        .style("background-color", colors[i])
+        .on("mouseenter", tooltipEnter)
+        .on("mousemove", tooltipMove)
+        .on("mouseleave", tooltipLeave)
+        .on("touchstart", tooltipEnter)
+        .on("touchmove", tooltipMove)
+        .on("touchend", tooltipLeave);
+    };
 
+    //this adds the age legends to the age histogram
     ageGroup.append('text').text(d => d.key)
         .style("font-size", "8px")
         .style("text-align", "right")
         .style("padding", "5px");
     
+    //this appends the right number of boxes to the ages histogram
     for (let i = 0; i < allAges.length; i++) {
         ageGroup
         .selectAll(".personAge")
@@ -208,41 +208,22 @@ d3.csv("berlin_wall_deaths.csv").then(function(dataset) {
         .enter()
         .append('div')
         .style("background-color", "#555555")
-        // .style("background-color", function(d) {
-        //     if (causes.indexOf(d.Cause) !== -1) {
-        //         let newIndex = causes.indexOf(d.Cause);
-        //         return colors[newIndex];
-        //     } else {
-        //         return "#ffffff";
-        //     }
-        // })
         .attr("class", "personAge");
     };
 
-   
-
+    //this appends the right number of boxes to the years histogram
     for (let i = 0; i < allYears.length; i++) {
         yearGroup
         .selectAll(".personYear")
         .data(d => d.values)
         .enter()
         .append('div')
-        // .style("background-color", function(d) {
-        //     if (causes.indexOf(d.Cause) !== -1) {
-        //         let newIndex = causes.indexOf(d.Cause);
-        //         return colors[newIndex];
-        //     } else {
-        //         return "#ffffff";
-        //     }
-        // })
         .style("background-color", "#555555")
         .attr("class", "personYear");
     }
-
+    //this adds the age legends to the year histogram
     yearGroup.append('text').text(d => d.key)
         .style("font-size", "8px")
         .style("text-align", "center");
-
-
 
 });
